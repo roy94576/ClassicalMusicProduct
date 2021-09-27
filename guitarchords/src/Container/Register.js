7d6e197b333e8a445bcd0d56a01a022b1d16a14c
roy94576 <roy94576@gmail.com>
2021-09-27 11:12:22 +0545

    Register

--- guitarchords/src/Container/Register.js
+++ guitarchords/src/Container/Register.js
@@ -3,38 +3,38 @@ import { Component } from "react";
 import axios from 'axios'
 
 class Register extends Component {
-    state = {
-        fname: "",
-        lname: "",
-        username: "",
-        email: "",
-        password: ""
-    }
+    // state = {
+    //     fname: "",
+    //     lname: "",
+    //     username: "",
+    //     email: "",
+    //     password: ""
+    // }
 
-    changeHandler = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-    }
+    // changeHandler = (e) => {
+    //     this.setState({
+    //         [e.target.name]: e.target.value
+    //     })
+    // }
 
-    sendUserdata = (e) => {
-        e.preventDefault()
-        const data = {
-            fname: this.state.fname,
-            lname: this.state.lname,
-            username: this.state.username,
-            email: this.state.email,
-            password: this.state.password,
-        }
-        axios.post("http://localhost:90/signup", data)
-            .then((response) => {
-                console.log(response)
-            }
-            )
-            .catch(error => {
-                console.log(error)
-            })
-    }
+    // sendUserdata = (e) => {
+    //     e.preventDefault()
+    //     const data = {
+    //         fname: this.state.fname,
+    //         lname: this.state.lname,
+    //         username: this.state.username,
+    //         email: this.state.email,
+    //         password: this.state.password,
+    //     }
+    //     axios.post("http://localhost:90/signup", data)
+    //         .then((response) => {
+    //             console.log(response)
+    //         }
+    //         )
+    //         .catch(error => {
+    //             console.log(error)
+    //         })
+    // }
     render() {
         return (
             <div className="container m-5">
@@ -45,27 +45,27 @@ class Register extends Component {
                         <form>
                             <div className="form-group">
                                 <label>First Name</label>
-                                <input type="fname" name="fname" value={this.state.fname} onChange={this.changeHandler} className="form-control" placeholder="Enter First Name" />
+                                <input type="fname" name="fname" className="form-control" placeholder="Enter First Name" />
                             </div>
                             <div className="form-group">
                                 <label>Last Name</label>
-                                <input type="lname" name="lname" value={this.state.lname} onChange={this.changeHandler} className="form-control" placeholder="Enter Last Name" />
+                                <input type="lname" name="lname" className="form-control" placeholder="Enter Last Name" />
                             </div>
                             <div className="form-group">
                                 <label>Username</label>
-                                <input type="username" name="username" value={this.state.username} onChange={this.changeHandler} className="form-control" placeholder="Enter Username" />
+                                <input type="username" name="username"  className="form-control" placeholder="Enter Username" />
                             </div>
                             <div className="form-group">
                                 <label>Email</label>
-                                <input type="email" name="email" value={this.state.email} onChange={this.changeHandler} className="form-control" placeholder="Enter Email" />
+                                <input type="email" name="email" className="form-control" placeholder="Enter Email" />
                             </div>
                             <div className="form-group">
                                 <label>Password</label>
-                                <input type="password" name="password" value={this.state.password} onChange={this.changeHandler} className="form-control" placeholder="Enter Password" />
+                                <input type="password" name="password" className="form-control" placeholder="Enter Password" />
                             </div>
                             <br></br>
 
-                            <button type="register" onClick={this.sendUserdata} className="btn btn-primary">Register</button>
+                            <button type="register" className="btn btn-primary">Register</button>
                         </form>
                     </div>
                 </div>
