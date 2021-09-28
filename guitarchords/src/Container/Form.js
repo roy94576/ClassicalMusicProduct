3dea3c117054b716a6ccbe27b611ca4851f70a76
roy94576 <roy94576@gmail.com>
2021-09-28 15:35:23 +0545

    Classical Music List

--- guitarchords/src/Container/Form.js
+++ guitarchords/src/Container/Form.js
@@ -2,57 +2,87 @@ import axios from "axios";
 import { Component } from "react";
 
 class Form extends Component {
-  state = {
-    name: "",
-    email: "",
-    phone: "",
-    classtype: ""
-  }
+  // state = {
+  //   name: "",
+  //   email: "",
+  //   phone: "",
+  //   classtype: ""
+  // }
 
-  makeBooking = (e) => {
-    e.preventDefault()
+  // makeBooking = (e) => {
+  //   e.preventDefault()
 
-    axios.post("http://localhost:90/makeBooking", this.state)
-      .then((response) => {
-        console.log(response)
-      }
-      )
-      .catch(error => {
-        console.log(error)
-      })
-  }
+  //   axios.post("http://localhost:90/makeBooking", this.state)
+  //     .then((response) => {
+  //       console.log(response)
+  //     }
+  //     )
+  //     .catch(error => {
+  //       console.log(error)
+  //     })
+  // }
 
   render() {
     return (
-      <div className="container m-5">
+     < div classname="container m-5">
 
-        <div class="card m-5">
-          <h5 className="card-header">Booking</h5>
-          <div className="card-body p-3">
-            <form>
-              <div class="form-row">
-                <div class="form-group col-md-6">
-                  <label> Name</label>
-                  <input type="name" value={this.state.name} onChange={(event) => { this.setState({ name: event.target.value }) }} className="form-control" placeholder="Enter Name" />
-                </div>
-              </div>
-              <div class="form-group">
-                <label>Email</label>
-                <input type="email" value={this.state.email} onChange={(event) => { this.setState({ email: event.target.value }) }} className="form-control" placeholder="Enter Email" />
-              </div>
-              <div class="form-group">
-                <label>Phone</label>
-                <input type="phone" value={this.state.phone} onChange={(event) => { this.setState({ phone: event.target.value }) }} className="form-control" placeholder="Enter Phone" />
-              </div>
-              <div class="form-group ">
-                <label>Class Type</label>
-                <textarea type="message" value={this.state.classtype} onChange={(event) => { this.setState({ classtype: event.target.value }) }} className="form-control" placeholder="Enter Class Type" />
-              </div>
-              <button type="submit" class="btn btn-primary" onClick={this.makeBooking}>Submit</button>
-            </form>
-          </div>
-        </div>
-      </div>
+            <div class="card m-5">
+                <h5 className="card-header">Medieval</h5>
+                <div className="card-body p-3">
+                <form></form>
+      <dl class="row">
+      <dt class="col-sm-3">Song</dt>
+      <dd class="col-sm-9">Mera Joota hai Jawani</dd>
+      <dt class="col-sm-3">Artist</dt>
+      <dd class="col-sm-9">
+        <p>Mukesh</p><button className="play" title="Play" value="Play" data-toggle="tooltip">Play</button>
+      </dd>
+      <dt class="col-sm-3">Album</dt>
+      <dd class="col-sm-9">Shree 420</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Song</dt>
+      <dd class="col-sm-9">Haal kaisa Hai Janab Ka</dd> 
+      <dt class="col-sm-3">Artist</dt>
+      <dd class="col-sm-9">
+        <p>Kishore Kumar Asha Bhosle</p><button className="play" title="Play" value="Play" data-toggle="tooltip">Play</button>
+      </dd>
+      <dt class="col-sm-3">Album</dt>
+      <dd class="col-sm-9">Chalti Ka Naam Gaadi</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Song</dt>
+      <dd class="col-sm-9">Bholi Surat Dil ke Khote</dd>
+      <dt class="col-sm-3">Artist</dt>
+      <dd class="col-sm-9">
+        <p>C.Ramachandran Lata Mangeskar</p><button className="play" title="Play" value="Play" data-toggle="tooltip">Play</button>
+      </dd>
+      <dt class="col-sm-3">Album</dt>
+      <dd class="col-sm-9">Albela</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Song</dt>
+      <dd class="col-sm-9">Suahan Safar</dd>
+      <dt class="col-sm-3">Artist</dt>
+      <dd class="col-sm-9">
+        <p>Mukesh</p><button className="play" title="Play" value="Play" data-toggle="tooltip">Play</button>
+      </dd>
+      <dt class="col-sm-3">Album</dt>
+      <dd class="col-sm-9">Madhumati</dd>
+    </dl>
+    <dl class="row">
+      <dt class="col-sm-3">Song</dt>
+      <dd class="col-sm-9">Yeh Hai Bombay Meri Jaan</dd>
+      <dt class="col-sm-3">Artist</dt>
+      <dd class="col-sm-9">
+        <p>Mohammad Rafi</p><button className="play" title="Play" value="Play" data-toggle="tooltip">Play</button>
+      </dd>
+      <dt class="col-sm-3">Album</dt>
+      <dd class="col-sm-9">CID</dd>
+    </dl>
+    </div>
+    </div>
+    </div>
     )
   }
 }
